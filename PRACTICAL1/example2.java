9230fa5106920475644050c012c54e1374b8a66c
Bhargavi1801 <bhargavibudida2007@gmail.com>
2025-08-04 20:46:07 +0530

    Add files via upload

---
+++ PRACTICAL1/example2.java
@@ -0,0 +1,13 @@
+package practical;
+
+public class example2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greeting = "Hello world!";
+		System.out.println(greeting);
+	}
+
+}
+
+
+
---
+++ PRACTICAL1/implicitcasting.java
@@ -0,0 +1,17 @@
+package practical;
+
+public class implicitcasting {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intNumber = 100;
+		long longNumber;
+		//implicit casting from int to long
+		longNumber = intNumber;
+        System.out.println("integer value:"+intNumber);
+        System.out.println("Long value after implicit casting:"+longNumber);
+	}
+
+}
+
+
+
